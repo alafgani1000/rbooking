2e74a0f22b2b68458bef144253c61783468ae7ac
alafgani1000 <irhabyaganal625@gmail.com>
2020-06-02 08:00:27 +0700

    add index js

--- index.js
+++ index.js
@@ -3,3 +3,26 @@ var url = require('url');
 var fs = require('fs');
 var path = require('path');
 var booking = require('./app/controller/BookingController.js');
+
+http.createServer(function (request, response){
+    response.writeHead(200, {'Content-Type': 'text/html'});
+	switch(request.url){
+        case '/':
+            fs.readFile('./view/frontend/index.html', (err, data) => {
+                if(err) throw err;
+                response.write(data);
+            });  
+            break;
+		case '/about':
+			response.write("ini adalah halaman about");
+			break;
+		case '/profile':
+			response.write("ini adalah halaman profile");
+			break;
+		default: 
+            response.write("404: Halaman tidak ditemukan");
+	}
+	return response.end();
+}).listen(8000);
+
+console.log('server runing on server 8000');
