d60f4b8e73d431efe7176da8c76a228ffb56d705
alafgani1000 <irhabyaganal625@gmail.com>
2020-07-17 07:28:32 +0700

    add routing

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var path = require('path');
-var booking = require('./app/controller/BookingController.js');
+'use strict';
 
-http.createServer(function (request, response){
-    response.writeHead(200, {'Content-Type': 'text/html'});
-	switch(request.url){
-        case '/':
-            fs.readFile('./view/frontend/index.html', (err, data) => {
-                if(err) throw err;
-                response.write(data);
-            });  
-            break;
-		case '/about':
-			response.write("ini adalah halaman about");
-			break;
-		case '/profile':
-			response.write("ini adalah halaman profile");
-			break;
-		default: 
-            response.write("404: Halaman tidak ditemukan");
-	}
-	return response.end();
-}).listen(8000);
+var express = require('express');
+var app = express();
+var logger = require('morgan');
 
-console.log('server runing on server 8000');
+app.use(logger('dev'));
+
+app.use(express.static(__dirname+'/publik'));
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+app.get('/profil', (req, res) => {
+    res.send('This is profile');
+});
+
+app.get('/contact', (req, res) => {
+    res.send('Contact in here');
+});
+
+app.listen(4000, function(){
+    console.log('Aplikasi sudsah berjalan di localhost:4000');
+});
